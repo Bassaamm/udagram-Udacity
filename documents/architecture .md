ea4003b2b4e3b5691eeee494b713b9a90598ec93
Bassaamm <bassamalqrni@gmail.com>
2023-12-23 11:55:47 +0300

    last update

---
+++ documents/architecture .md
@@ -0,0 +1,7 @@
+# Architecture
+
+### The Front-end hosted using S3 service: http://bas-bucket-udagram.s3-website-us-east-1.amazonaws.com/
+
+### The Back-end hosted using ElasticBeansatalk: http://udagram-api-dev.eba-fsggp5ve.us-east-1.elasticbeanstalk.com/api/v0
+
+### In the following picture it will have aws services used to deploy the full stack application
--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=Aa11223
+export POSTGRES_PASSWORD=Aa112233
 export POSTGRES_HOST=database-1.crrpbvktsrdq.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::bas-bucket-udagram
