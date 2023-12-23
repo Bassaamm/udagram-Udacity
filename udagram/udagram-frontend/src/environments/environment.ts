7f76d1b023a1fd260df4fc9b946e6eaed89c4ae2
Bassaamm <bassamalqrni@gmail.com>
2023-12-23 10:56:33 +0300

    update apihost

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: "Udagram",
   apiHost:
-    "http://udagram-api-dev.eba-fsggp5ve.us-east-1.elasticbeanstalk.com/api/v0",
+    "http://udagram-api-dev.eba-fsggp5ve.us-east-1.elasticbeanstalk.com/zapi/v0",
 };
 
 /*
